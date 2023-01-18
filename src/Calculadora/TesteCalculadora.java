c8bd344ca9c4b521521054d63f7c7d285f9523a8
GLDTRA <guga_floriano13@hotmail.com>
2023-01-18 16:47:45 -0300

    feat: implementation on class TesteCalculadora

--- src/Calculadora/TesteCalculadora.java
+++ src/Calculadora/TesteCalculadora.java
@@ -1,29 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TesteCalculadora {
     public static void main(String[] args) {
-        int opcao1=0, opcao2, num, a, b;
         float num1, num2, resultado=0;
+
+        ArrayList<Integer> opcao = new ArrayList<>();
+
         Scanner ler = new Scanner(System.in);
         System.out.println("Bem vindo a nossa calculadora!!\n");
         do{
-            System.out.println("Digite um número de 1 ou 2, sendo:\n1 - Operações básicas\n2 - Sair\n");//to do escrever
-            opcao1 = ler.nextInt();
-            if(opcao1 == 1){
+
+            if(opcao.size() == 0){
+                System.out.println("Digite um número de 1 ou 2, sendo:\n1 - Operações básicas\n2 - Sair\n");//to do escrever
+                opcao.add(0, ler.nextInt());
+            }
+            else{
+                System.out.println("Digite um número de 1 ou 2, sendo:\n1 - Operações básicas\n2 - Sair\n");//to do escrever
+                opcao.set(0, ler.nextInt());
+            }
+
+            if(opcao.get(0) == 1){
                 System.out.println("Digite um numero para as operações matemáticas:" +
                         "\n1 - soma" +
                         "\n2 - subtração" +
                         "\n3 - multiplicação" +
                         "\n4 - Divisão");
-                opcao2 = ler.nextInt();
-                switch (opcao2){
+                opcao.add(1, ler.nextInt());
+                switch (opcao.get(1)){
                     case 1:
                         System.out.println("Digite um 1o numero:");
                         num1 = ler.nextFloat();
                         System.out.println("Digite um 2o numero:");
                         num2 = ler.nextFloat();
-//                        OperacaoBasica opBas = new OpercaoBasica(num1, num2);
-//                        resultado = opBas.Soma(num1, num2);
+                        OperacaoBasica opBas = new OperacaoBasica(num1, num2);
+                        resultado = opBas.Soma();
                         System.out.println("Número "+num1+" + "+num2+" = "+resultado);
                         break;
                     case 2:
@@ -31,8 +42,8 @@ public class TesteCalculadora {
                         num1 = ler.nextFloat();
                         System.out.println("Digite um 2o numero:");
                         num2 = ler.nextFloat();
-//                        operacaoBasica OpBas = new opercaoBasica(num1, num2);
-//                        resultado = OpBas.Diminuir(num1, num2);
+                        OperacaoBasica OpBas = new OperacaoBasica(num1, num2);
+                        resultado = OpBas.Diminuir();
                         System.out.println("Número "+num1+" - "+num2+" = "+resultado);
                         break;
                     case 3:
@@ -40,8 +51,8 @@ public class TesteCalculadora {
                         num1 = ler.nextFloat();
                         System.out.println("Digite um 2o numero:");
                         num2 = ler.nextFloat();
-//                        operacaoBasica OpBas = new opercaoBasica(num1, num2);
-//                        resultado = OpBas.Multiplicar(num1, num2);
+                        OperacaoBasica OpBas1 = new OperacaoBasica(num1, num2);
+                        resultado = OpBas1.Multiplicar();
                         System.out.println("Número "+num1+" x "+num2+" = "+resultado);
                         break;
                     case 4:
@@ -49,20 +60,21 @@ public class TesteCalculadora {
                         num1 = ler.nextFloat();
                         System.out.println("Digite um 2o numero:");
                         num2 = ler.nextFloat();
-//                        operacaoBasica OpBas = new opercaoBasica(dim1, dim2);
-//                        resultado = OpBas.Dividir(num1, num2);
+                        OperacaoBasica OpBas2 = new OperacaoBasica(num1, num2);
+                        resultado = OpBas2.Dividir();
                         System.out.println("Número "+num1+" / "+num2+" = "+resultado);
                         break;
                     default:
                         System.out.println("Operação inválida!");
                 }
             System.out.println("Você deseja continuar?\n1 - Voltar para o menu inicial\n2 - Sair");
-            opcao1 = ler.nextInt();
+            opcao.set(0, ler.nextInt());
             }
-            else if (opcao1 != 2)System.out.println("Digite outro numero!");
+            else if (opcao.get(0) != 2)System.out.println("Digite outro numero!");
             else {
                 System.out.println("Calculadora foi finalizada com sucesso, obrigada!");
             }
-        }while(opcao1 != 2);
+        }while(opcao.get(0) != 2);
+        System.out.println("\n**   Muito Obrigado por usar nosso programa!   **\n\n**   Até breve!   **");
     }
 }
