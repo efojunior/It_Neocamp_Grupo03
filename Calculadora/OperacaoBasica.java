3d1d18aaf187b5a6c0fe97f0fc0ee76e852df1c8
jufaria <julio.faria@mercadolivre.com>
2023-01-17 21:38:57 -0300

    Classe OperacaoBasica

--- Calculadora/OperacaoBasica.java
+++ Calculadora/OperacaoBasica.java
@@ -1,3 +1,45 @@
 public class OperacaoBasica {
-  
+
+    private float num1, num2, resultado;
+
+    public float Soma(){
+       this.resultado=this.num1 + this.num2;
+       return resultado
+    }
+    public float Diminuir(){
+        this.resultado=this.num1 - this.num2;
+        return resultado
+    }
+    public float Multiplicar(){
+        this.resultado=this.num1 * this.num2;
+        return resultado
+    }
+    public float Dividir(){
+        this.resultado=this.num1 / this.num2;
+        return resultado
+    }
+
+    public OperacaoBasica(float num1, float num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+
+    }
+
+    public float getNum1() {
+        return num1;
+    }
+
+    public void setNum1(float num1) {
+        this.num1 = num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
+
+    public void setNum2(float num2) {
+        this.num2 = num2;
+    }
+
+
 }
